e061f43d26bcb6857a7bb4fd44d2b42dd77b80d7
Lauri Peltonen <30770072+zanppa@users.noreply.github.com>
2022-10-02 14:27:58 +0300

    Remove ADC1 rdson current measurement triggering from svm.c as it is now done automatically

--- Src/svm.c
+++ Src/svm.c
@@ -181,15 +181,6 @@ void TIM1_UP_IRQHandler() {
   // DEBUG: LED on
   //HAL_GPIO_TogglePin(LED_PORT,LED_PIN);
 
-// TODO: Debug: moved to bldc.c
-// Trigger RDSon measurement here
-//  if(!(TIM1->CR1 & TIM_CR1_DIR)) {
-    // Trigger ADC rdson measurement when we have 000 zero vector
-    // TODO: Use automatic trigger from timer?
-//#if defined(I_MEAS_RDSON)
-//    adc_rdson.Instance->CR2 |= ADC_CR2_SWSTART;
-//#endif
-  //}// else return; // TODO: Debug, only do SVM calculations on upcounting interrupt TODO: This did not help!
 
 #if defined(LEFT_MOTOR_SVM)
   mode = motor_state[STATE_LEFT].ref.control_mode;
