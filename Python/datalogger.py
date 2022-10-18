056898348c8776f89d84900646559f7ddaba626f
Lauri Peltonen <30770072+zanppa@users.noreply.github.com>
2022-10-18 22:45:07 +0300

    Update datalogger example

--- Python/datalogger.py
+++ Python/datalogger.py
@@ -38,17 +38,25 @@ while not (ct & 2):
 
 
 print("Reading data...")
-print("Index,Data0,Data1,Data2,Data3")
+print("Index,Data0,Data1,Data2,Data3,Data4,Data5,Data6,Data7")
 for i in range(256):
-  addr = (255-i) * 4 # Last index is the first stored value. Keep low 2 bytes as zero to not trigger again
+  addr = (255-i) * 2 * 4 # Last index is the first stored value. Keep low 2 bits zero to not trigger again
+
+  # Read first 4 channels
   instrument.write_register(ctrl_reg, swap(addr), 0)
   data = instrument.read_registers(data_reg, 4)
-  for j in range(4):
+
+  # Read last 4 channels
+  instrument.write_register(ctrl_reg, swap(addr + (1*4)), 0) # offset times 4 to keep the low 2 bits zero
+  data2 = instrument.read_registers(data_reg, 4)
+  data.extend(data2)
+
+  for j in range(8):
     data[j] = swap(data[j])
 
   # Handle data type dependent magic here
-  data[1] = signed(data[1])
   data[2] = signed(data[2])
+  data[3] = signed(data[3])
 
   # Insert index as the first column
   data.insert(0, i)
